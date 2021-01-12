3959ad1c8ed53eb5146ca6b0708912fc3752410b
Nomad_Hash <badsoap141@gmail.com>
2021-01-12 23:22:22 +0900

    feature / project / set directory and global styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
-
+// * =====================
+// *   IMPORT_CONTAINERS
+// * =====================
+import LandingPageContainer from './containers/pages/landing_page/LandingPageContainer';
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LandingPageContainer />
+    </div>
+  );
 };
 
 export default App;
