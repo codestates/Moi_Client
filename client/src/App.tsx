683ead1fa7791fc81064a2f17436a2a51307bc1c
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-01-13 17:37:49 +0900

    feture/landing_page/create_intro

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 // *   IMPORT_CONTAINERS
 // * =====================
 import HeaderContainer from './containers/systems/header/HeaderContainer';
+import LandingPageContainer from './containers/pages/landing_page/LandingPageContainer';
 const App: React.FC = () => {
   return (
     <div className="App">
       <HeaderContainer />
+      <LandingPageContainer />
     </div>
   );
 };
---
+++ client/src/components/pages/landing_page/Intro/right_preview/RightPreview.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from '../../../../../styles/pages/landing_page/Intro/Intro.module.css';
+
+const RightPreview: React.FC = () => {
+  return (
+    <div className={styles.right__menu}>
+      <article className={styles.right__gif}>
+        <img
+          className={styles.img}
+          src="https://media.giphy.com/media/l4pT7j0OAUCvtWXXG/source.gif"
+          alt="gif"
+        />
+      </article>
+    </div>
+  );
+};
+
+export default RightPreview;
