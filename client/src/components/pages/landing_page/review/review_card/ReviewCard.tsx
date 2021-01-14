478bfb9d53b69a99a511fd405682652aae7ac614
Nomad_Hash <badsoap141@gmail.com>
2021-01-14 15:27:46 +0900

    refactor / landing_page / refactor review section

--- client/src/components/pages/landing_page/review/review_card/ReviewCard.tsx
+++ client/src/components/pages/landing_page/review/review_card/ReviewCard.tsx
@@ -1,112 +1,42 @@
 import React from 'react';
 import styles from '../../../../../styles/pages/landing_page/review/ReviewCard.module.css';
 import { FaStar } from 'react-icons/fa';
-import avatar from '../../../../../public/avatar1.png';
+import { reviews } from '../../../../../data/landing_page/reviews/data';
 
 const ReviewCard: React.FC = () => {
-  return (
-    <ul className={styles.review__card__list}>
-      <li className={styles.review__card__list_item}>
+  const starIconMaker: (rating: number) => JSX.Element[] = (rating: number) => {
+    const starList = new Array(rating).fill(5);
+    return starList.map((_, index) => {
+      return (
+        <li key={index}>
+          <FaStar />
+        </li>
+      );
+    });
+  };
+
+  const reviewMaker = reviews.map((element) => {
+    return (
+      <li className={styles.review__card__list_item} key={element.name}>
         <header>
-          <img src={avatar} alt="preview_avatar" />
+          <img src={element.profileImg} alt="preview_avatar" />
           <div className={styles.review__card__list__header__rating__block}>
-            <strong>박준형</strong>
+            <strong>{element.name}</strong>
             <ul className={styles.review__card__list__star__list}>
-              <li>
-                <FaStar />
-              </li>
-              <li>
-                <FaStar />
-              </li>
-              <li>
-                <FaStar />
-              </li>
-              <li>
-                <FaStar />
-              </li>
-              <li>
-                <FaStar />
-              </li>
+              {starIconMaker(element.rating)}
             </ul>
           </div>
         </header>
-        <article>
-          확실히 CV 이력서 작성기를 사용하길 잘 했다는 생각이 듭니다. 단 15분
-          만에 이력서를 완성하여 이메일 프로그램을 통해 전송할 수 있었습니다.
-        </article>
+        <article>{element.description}</article>
         <br />
         <br />
         <strong>구직 분야</strong>
-        <span>소프트웨어 개발</span>
+        <span>{element.role}</span>
       </li>
-      <li className={styles.review__card__list_item}>
-        <header>
-          <img src={avatar} alt="preview_avatar" />
-          <div className={styles.review__card__list__header__rating__block}>
-            <strong>박준형</strong>
-            <ul className={styles.review__card__list__star__list}>
-              <li>
-                <FaStar />
-              </li>
-              <li>
-                <FaStar />
-              </li>
-              <li>
-                <FaStar />
-              </li>
-              <li>
-                <FaStar />
-              </li>
-              <li>
-                <FaStar />
-              </li>
-            </ul>
-          </div>
-        </header>
-        <article>
-          확실히 CV 이력서 작성기를 사용하길 잘 했다는 생각이 듭니다. 단 15분
-          만에 이력서를 완성하여 이메일 프로그램을 통해 전송할 수 있었습니다.
-        </article>
-        <br />
-        <br />
-        <strong>구직 분야</strong>
-        <span>소프트웨어 개발</span>
-      </li>
-      <li className={styles.review__card__list_item}>
-        <header>
-          <img src={avatar} alt="preview_avatar" />
-          <div className={styles.review__card__list__header__rating__block}>
-            <strong>박준형</strong>
-            <ul className={styles.review__card__list__star__list}>
-              <li>
-                <FaStar />
-              </li>
-              <li>
-                <FaStar />
-              </li>
-              <li>
-                <FaStar />
-              </li>
-              <li>
-                <FaStar />
-              </li>
-              <li>
-                <FaStar />
-              </li>
-            </ul>
-          </div>
-        </header>
-        <article>
-          확실히 CV 이력서 작성기를 사용하길 잘 했다는 생각이 듭니다. 단 15분
-          만에 이력서를 완성하여 이메일 프로그램을 통해 전송할 수 있었습니다.
-        </article>
-        <br />
-        <br />
-        <strong>구직 분야</strong>
-        <span>소프트웨어 개발</span>
-      </li>
-    </ul>
-  );
+    );
+  });
+
+  return <ul className={styles.review__card__list}>{reviewMaker}</ul>;
 };
 
 export default ReviewCard;
---
+++ client/src/data/landing_page/reviews/data.ts
@@ -0,0 +1,29 @@
+export const reviews = [
+  {
+    profileImg:
+      'http://carismartes.com.br/assets/global/images/avatars/avatar1_big.png',
+    name: '박준형',
+    rating: 5,
+    description:
+      '확실히 CV 이력서 작성기를 사용하길 잘 했다는 생각이 듭니다 단 15분만에 이력서를 완성하여 이메일 프로그램을 통해 전송할 수 있었습니다.',
+    role: '소프트웨어 개발',
+  },
+  {
+    profileImg:
+      'http://carismartes.com.br/assets/global/images/avatars/avatar3_big.png',
+    name: '김준형',
+    rating: 5,
+    description:
+      '이력서에 대한 칭찬을 많이 받았고 단시간에 좋은 직장을 구할 수 있었습니다. CV 이력서 작성기를 강력하게 추천합니다!',
+    role: '데이터 분석',
+  },
+  {
+    profileImg:
+      'http://carismartes.com.br/assets/global/images/avatars/avatar12_big.png',
+    name: '이준형',
+    rating: 5,
+    description:
+      'CV 이력서 작성기를 사용하여 모든 이력서와 앱을 한 곳에 모아볼 수 있다는 점이 아주 편리하다고 생각합니다. 훌륭한 템플릿이 정말 많습니다!',
+    role: '그래픽 디자인 ',
+  },
+];
