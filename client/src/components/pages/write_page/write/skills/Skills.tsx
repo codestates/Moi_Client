e15c7afc9ad2e143471a1c0404aa49fa25fd99e0
Nomad_Hash <badsoap141@gmail.com>
2021-01-19 14:48:54 +0900

    feature/write_page/create add and delete skill field actions

--- client/src/components/pages/write_page/write/skills/Skills.tsx
+++ client/src/components/pages/write_page/write/skills/Skills.tsx
@@ -1,23 +1,41 @@
-import React, { useState } from 'react';
-import styles from '../../../../../styles/pages/write_page/write/Write.module.css';
+import React from 'react';
+import { SkillItem } from '../../../../../modules/changeField/skills/types';
 import List from './list/List';
+import styles from '../../../../../styles/pages/write_page/write/Write.module.css';
+
 interface SkillProps {
   addSkill: () => void;
+  skills: SkillItem[];
+  onChangeSkillFields: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onDeleteSkillFields: (index: number) => void;
 }
-const Skills: React.FC<SkillProps> = ({ addSkill }) => {
-  const [listAdd, setListAdd] = useState<boolean>(false);
-  const onListAdd = (): void => {
-    addSkill();
-    setListAdd(!listAdd);
-  };
+
+const Skills: React.FC<SkillProps> = ({
+  addSkill,
+  skills,
+  onChangeSkillFields,
+  onDeleteSkillFields,
+}) => {
+  const list = skills.map((ele, index) => {
+    return (
+      <List
+        key={index}
+        skill={ele.skill}
+        desc={ele.desc}
+        index={index}
+        onChangeSkillFields={onChangeSkillFields}
+        onDeleteSkillFields={onDeleteSkillFields}
+      />
+    );
+  });
 
   return (
     <article className={styles.skills_container}>
       <div className={styles.skills_title}>기술</div>
       <div className={styles.skills_button}>
-        <button onClick={onListAdd}>+ 추가</button>
+        <button onClick={addSkill}>+ 추가</button>
       </div>
-      {listAdd ? <List /> : null}
+      {list}
     </article>
   );
 };
